1812e4ce590a713e7ee5254db5bbda1adbabe19d
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2021-06-15 13:50:31 +0100

    Removing logs to investigate vat url issue

--- app/connectors/DesConnector.scala
+++ app/connectors/DesConnector.scala
@@ -34,7 +34,6 @@ class DesConnector @Inject()(des: DesConfig, httpClient: HttpClient)
       .withExtraHeaders("Environment" -> des.environment)
 
   def getVatCustomerDetails(vrn: Vrn): Future[VatCustomerInfoResponse] = {
-    logger.warn(s"Test URLTest ${des.baseUrl}vat/customer/vrn/${vrn.value}/information")
     httpClient.GET[VatCustomerInfoResponse](s"${des.baseUrl}vat/customer/vrn/${vrn.value}/information")
   }
 }
