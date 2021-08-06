9c55be93c43203cc59a75d1c5375dcc2e9054573
ansahdavid <45106722+ansahdavid@users.noreply.github.com>
2021-08-06 15:04:44 +0100

    Added test and impl for collection backup

---
+++ app/repositories/RegistrationBackUpRepository.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package repositories
+
+import config.AppConfig
+import crypto.RegistrationEncrypter
+import logging.Logging
+import models.EncryptedRegistration
+import org.mongodb.scala.model.{IndexModel, IndexOptions, Indexes}
+import uk.gov.hmrc.mongo.MongoComponent
+import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
+
+import java.time.Clock
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class RegistrationBackUpRepository @Inject()(
+                                              mongoComponent: MongoComponent
+                                            )(implicit ec: ExecutionContext)
+  extends PlayMongoRepository[EncryptedRegistration] (
+    collectionName = "registrations-backup",
+    mongoComponent = mongoComponent,
+    domainFormat   = EncryptedRegistration.format,
+    replaceIndexes = true,
+    indexes        = Seq(
+      IndexModel(
+        Indexes.ascending("vrn"),
+        IndexOptions()
+          .name("vrnIndex")
+          .unique(true)
+      )
+    )
+  ) with Logging {
+
+  def insertMany(encryptedRegistrations: List[EncryptedRegistration]): Future[Boolean] = {
+    encryptedRegistrations match {
+      case Nil => Future.successful(true)
+      case _ =>
+        collection.insertMany(encryptedRegistrations).toFuture.map(_ => true).recover {
+          case ex =>
+            logger.error(s"Failed to insert many Encrypted Registrations ${ex.getMessage}")
+            false
+        }
+    }
+  }
+}
