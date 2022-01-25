64ffb3c8afcb7516ad51c5005b3f691a92074549
oliwiaKot <48218839+oliwiaKot@users.noreply.github.com>
2022-01-25 15:30:55 +0000

    VOESS-800| updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,23 +6,23 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.4.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.50.0",
-    "uk.gov.hmrc"             %% "domain"                     % "5.11.0-play-27",
-    "org.typelevel"           %% "cats-core"                  % "2.6.1"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.20.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.59.0",
+    "uk.gov.hmrc"             %% "domain"                     % "7.0.0-play-28",
+    "org.typelevel"           %% "cats-core"                  % "2.7.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.2.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.50.0",
-    "org.scalatest"           %% "scalatest"                  % "3.2.5",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.20.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.59.0",
+    "org.scalatest"           %% "scalatest"                  % "3.2.10",
     "com.typesafe.play"       %% "play-test"                  % PlayVersion.current,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8",
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2",
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
-    "org.scalatestplus"       %% "mockito-3-4"                % "3.2.7.0",
-    "org.mockito"             %% "mockito-scala"              % "1.16.0",
-    "com.github.tomakehurst"  %  "wiremock-standalone"        % "2.25.0",
-    "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.7.0",
-    "org.scalacheck"          %% "scalacheck"                 % "1.15.3"
+    "org.scalatestplus"       %% "mockito-3-4"                % "3.2.10.0",
+    "org.mockito"             %% "mockito-scala"              % "1.17.0",
+    "com.github.tomakehurst"  %  "wiremock-standalone"        % "2.27.2",
+    "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.11.0",
+    "org.scalacheck"          %% "scalacheck"                 % "1.15.4"
   ).map(_ % "test, it")
 }
