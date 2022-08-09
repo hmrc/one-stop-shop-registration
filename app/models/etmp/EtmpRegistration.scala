86b552782735351b8dbd4d760fedd0bc9fab98c7
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2022-08-09 15:44:05 +0100

    VEOSS-1177 | Align registration submission model to ETMP

--- app/models/etmp/EtmpRegistration.scala
+++ app/models/etmp/EtmpRegistration.scala
@@ -1,25 +1,53 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package models.etmp
 
-import models.{BankDetails, ContactDetails, EuTaxRegistration, NiPresence, PreviousRegistration, VatDetails}
+import models.{BankDetails, Registration}
+import play.api.libs.json.{Json, OFormat}
 import uk.gov.hmrc.domain.Vrn
-
-import java.time.{Instant, LocalDate}
 
 case class EtmpRegistration(
                              vrn: Vrn,
                              tradingNames: Seq[EtmpTradingNames],
-                             vatDetails: VatDetails,
-                             schemaDetails: EtmpSchemeDetails,
-                             euRegistrations: Seq[EuTaxRegistration],
-                             contactDetails: ContactDetails,
-                             websites: Seq[String],
-                             commencementDate: LocalDate,
-                             previousRegistrations: Seq[PreviousRegistration],
-                             bankDetails: BankDetails,
-                             isOnlineMarketplace: Boolean,
-                             niPresence: Option[NiPresence],
-                             dateOfFirstSale: Option[LocalDate],
-                             submissionReceived: Instant,
-                             lastUpdated: Instant
+                             schemeDetails: EtmpSchemeDetails,
+                             bankDetails: BankDetails
                            ) {
 }
+
+object EtmpRegistration {
+
+  def fromRegistration(registration: Registration): EtmpRegistration = {
+    EtmpRegistration(
+      vrn = registration.vrn,
+      tradingNames = registration.tradingNames.map(EtmpTradingNames(_)),
+      schemeDetails = EtmpSchemeDetails(
+        commencementDate = registration.commencementDate.format(EtmpSchemeDetails.dateFormatter),
+        firstSaleDate = registration.dateOfFirstSale.map(_.format(EtmpSchemeDetails.dateFormatter)),
+        euRegistrationDetails = Some(registration.euRegistrations.map(registration => EtmpEuRegistrationDetails.create(registration))),
+        previousEURegistrationDetails = Some(registration.previousRegistrations.map(previous => EtmpPreviousEURegistrationDetails(previous.country.code, previous.vatNumber))),
+        onlineMarketPlace = registration.isOnlineMarketplace,
+        websites = registration.websites.map(Website(_)),
+        contactName = registration.contactDetails.fullName,
+        businessTelephoneNumber = registration.contactDetails.telephoneNumber,
+        businessEmailId = registration.contactDetails.emailAddress),
+      bankDetails = registration.bankDetails
+    )
+  }
+
+  implicit val format: OFormat[EtmpRegistration] = Json.format[EtmpRegistration]
+}
+
