47dabd13e69e16a434d716868ac94deeacee2b56
DevAndyroo <andy@andyroo.info>
2022-10-19 15:53:15 +0100

    VEOSS-1301 alter vat info model to always expect a part of vat group

--- test/models/des/VatCustomerInfoSpec.scala
+++ test/models/des/VatCustomerInfoSpec.scala
@@ -18,27 +18,27 @@ class VatCustomerInfoSpec extends BaseSpec {
           "approvedInformation" -> Json.obj(
             "PPOB" -> Json.obj(
               "address" -> Json.obj(
-                "line1"       -> "line 1",
-                "line2"       -> "line 2",
-                "line3"       -> "line 3",
-                "line4"       -> "line 4",
-                "line5"       -> "line 5",
-                "postCode"    -> "postcode",
+                "line1" -> "line 1",
+                "line2" -> "line 2",
+                "line3" -> "line 3",
+                "line4" -> "line 4",
+                "line5" -> "line 5",
+                "postCode" -> "postcode",
                 "countryCode" -> "CC"
               )
             ),
             "customerDetails" -> Json.obj(
               "effectiveRegistrationDate" -> "2020-01-02",
-              "partyType"                 -> "Z2",
-              "organisationName"          -> "Foo"
+              "partyType" -> "Z2",
+              "organisationName" -> "Foo"
             )
           )
         )
 
         val expectedResult = VatCustomerInfo(
-          address          = DesAddress("line 1", Some("line 2"), Some("line 3"), Some("line 4"), Some("line 5"), Some("postcode"), "CC"),
+          address = DesAddress("line 1", Some("line 2"), Some("line 3"), Some("line 4"), Some("line 5"), Some("postcode"), "CC"),
           registrationDate = Some(LocalDate.of(2020, 1, 2)),
-          partOfVatGroup   = Some(true),
+          partOfVatGroup = true,
           organisationName = Some("Foo")
         )
 
@@ -51,27 +51,27 @@ class VatCustomerInfoSpec extends BaseSpec {
           "approvedInformation" -> Json.obj(
             "PPOB" -> Json.obj(
               "address" -> Json.obj(
-                "line1"       -> "line 1",
-                "line2"       -> "line 2",
-                "line3"       -> "line 3",
-                "line4"       -> "line 4",
-                "line5"       -> "line 5",
-                "postCode"    -> "postcode",
+                "line1" -> "line 1",
+                "line2" -> "line 2",
+                "line3" -> "line 3",
+                "line4" -> "line 4",
+                "line5" -> "line 5",
+                "postCode" -> "postcode",
                 "countryCode" -> "CC"
               )
             ),
             "customerDetails" -> Json.obj(
               "effectiveRegistrationDate" -> "2020-01-02",
-              "partyType"                 -> "ZZ",
-              "organisationName"          -> "Foo"
+              "partyType" -> "ZZ",
+              "organisationName" -> "Foo"
             )
           )
         )
 
         val expectedResult = VatCustomerInfo(
-          address          = DesAddress("line 1", Some("line 2"), Some("line 3"), Some("line 4"), Some("line 5"), Some("postcode"), "CC"),
+          address = DesAddress("line 1", Some("line 2"), Some("line 3"), Some("line 4"), Some("line 5"), Some("postcode"), "CC"),
           registrationDate = Some(LocalDate.of(2020, 1, 2)),
-          partOfVatGroup   = Some(false),
+          partOfVatGroup = false,
           organisationName = Some("Foo")
         )
 
@@ -84,7 +84,7 @@ class VatCustomerInfoSpec extends BaseSpec {
           "approvedInformation" -> Json.obj(
             "PPOB" -> Json.obj(
               "address" -> Json.obj(
-                "line1"       -> "line 1",
+                "line1" -> "line 1",
                 "countryCode" -> "CC"
               )
             ),
@@ -93,9 +93,9 @@ class VatCustomerInfoSpec extends BaseSpec {
         )
 
         val expectedResult = VatCustomerInfo(
-          address          = DesAddress("line 1", None, None, None, None, None, "CC"),
+          address = DesAddress("line 1", None, None, None, None, None, "CC"),
           registrationDate = None,
-          partOfVatGroup   = None,
+          partOfVatGroup = false,
           organisationName = None
         )
 
