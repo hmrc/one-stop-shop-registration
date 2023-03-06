1fe387f5e00be63b2941ecd6edba36ee0808dece
Warren Smithson <36073378+warrenskiwork1979@users.noreply.github.com>
2023-03-06 11:06:55 +0000

    Fix import issues following merge conflicts

--- app/controllers/test/TestOnlyController.scala
+++ app/controllers/test/TestOnlyController.scala
@@ -18,7 +18,7 @@ package controllers.test
 
 import models.EuTaxIdentifierType.Vat
 import models.VatDetailSource.UserEntered
-import models.{BankDetails, ContactDetails, Country, DesAddress, EuTaxIdentifier, Iban, InternationalAddress, PreviousRegistrationLegacy, PreviousRegistrationNew, PreviousScheme, PreviousSchemeDetails, PreviousSchemeNumbers, PrincipalPlaceOfBusinessInNi, Registration, RegistrationWithFixedEstablishment, RegistrationWithoutFixedEstablishment, TradeDetails, VatDetails}
+import models.{BankDetails, ContactDetails, Country, DesAddress, EuTaxIdentifier, Iban, InternationalAddress, PreviousRegistrationLegacy, PreviousRegistrationNew, PreviousScheme, PreviousSchemeDetails, PreviousSchemeNumbers, PrincipalPlaceOfBusinessInNi, Registration, RegistrationWithFixedEstablishment, RegistrationWithoutTaxId, TradeDetails, VatDetails}
 import org.mongodb.scala.model.Filters
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import repositories.RegistrationRepository
