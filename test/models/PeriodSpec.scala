762fa22f4f8f786b335392a22f1b1d6312366fbd
Susan Swinburne <susan.badel@opencastsoftware.com>
2025-07-16 13:04:13 +0100

    VEOSS-2115 update payment deadline in test

--- test/models/PeriodSpec.scala
+++ test/models/PeriodSpec.scala
@@ -51,7 +51,8 @@ class PeriodSpec extends BaseSpec with ScalaCheckPropertyChecks {
     }
 
     "must return false if the payment deadline has not passed" in {
-      val period = StandardPeriod(2025, Q1)
+      val nextYear = LocalDate.now(stubClock).plusYears(1).getYear
+      val period = StandardPeriod(nextYear, Q1)
 
       period.isOverdue(stubClock) mustBe false
     }
