10046aa722b5982a91e92cea38586fe48ba8f54a
Spencer Clarke-Griffiths <130156132+SpencerCGriffiths@users.noreply.github.com>
2025-09-09 12:59:19 +0100

    VEOSS-2129 Bug Fix- Retroactively set LastUpdated for old documents (#197)
    * VEOSS-2129 Implement CRON Actor Scheduler to implement LastUpdated for documents
    
    * VEOSS-2129 Implement a feature switch for Actor System
    
    * VEOSS-2129 Increasing test time to rule out race condition in PR Builder test
    
    * VEOSS-2129 Update to use Transaction
    
    * VEOSS-2129 update test naming
    
    * VEOSS-2129 Implements ActorSystem as a Service bound to Module
    
    * VEOSS-2129 Removing println
    
    * VEOSS-2129 resolves flaky tests
    
    * VEOSS-2129 Extends test time to improve reliability
    
    * VEOSS-2129 LastUpdated setting as default and allowing test more time to run

---
+++ app/services/cron/CronService.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services.cron
+
+import config.AppConfig
+import logging.Logging
+import org.apache.pekko.actor.ActorSystem
+import repositories.RegistrationStatusRepository
+
+import javax.inject.*
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.*
+
+trait CronService
+
+class CronServiceImpl @Inject()(
+                                 system: ActorSystem,
+                                 appConfig: AppConfig,
+                                 registrationStatusRepository: RegistrationStatusRepository
+                               )(implicit ec: ExecutionContext) extends Logging with CronService {
+
+  system.scheduler.scheduleOnce(
+    delay = appConfig.delay.microseconds
+  ) {
+    if (appConfig.lastUpdatedFeatureSwitch) {
+      registrationStatusRepository.fixAllDocuments().map { entriesChanged =>
+        logger.info(s"Implementing TTL: ${entriesChanged.size} documents were read as last updated Instant.now and set to current date & time.")
+      }.recover{
+        case e => logger.error(e.getMessage, e)
+      }
+    } else {
+      logger.info("ExpiryScheduler disabled; not starting.")
+    }
+  }
+}
